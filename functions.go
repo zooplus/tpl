addc67717b5ee263741cb4ae297e65768292df64
Tomasz Gagor <tomasz.gagor@zooplus.com>
2026-02-11 00:21:13 +0100

    refactor: move functions from global scope to processor scoped

---
+++ functions.go
@@ -0,0 +1,18 @@
+package main
+
+import "text/template"
+
+func (tp *TemplateProcessor) funcMap() template.FuncMap {
+	return template.FuncMap{
+		"include":     tp.include,
+		"mustInclude": tp.mustInclude,
+	}
+}
+
+func (tp *TemplateProcessor) include(fileName string) (string, error) {
+	return tp.renderInclude(fileName, true)
+}
+
+func (tp *TemplateProcessor) mustInclude(fileName string) (string, error) {
+	return tp.renderInclude(fileName, false)
+}
