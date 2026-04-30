15231526d61212aad2f626d115a4d56658395bfb
Tomasz Gagor <tomasz.gagor@zooplus.com>
2026-04-30 15:27:55 +0200

    fix: improve handling defer and os.Exit to avoid leaking resources

--- main.go
+++ main.go
@@ -2,42 +2,54 @@ package main
 
 import (
 	"fmt"
-	"github.com/tgagor/tpl/tpl"
 	"os"
+
+	"github.com/zooplus/tpl/tpl"
 )
 
 var BuildVersion = "development" // Fallback if not set during build
 
 // control ALL exits from main
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
 	config := tpl.ParseFlags()
 	logger := tpl.NewLogger(config.Debug)
 
 	if config.Version {
 		fmt.Printf("version %s\n", BuildVersion)
-		os.Exit(0)
+		return 0
 	}
 
 	if len(config.TemplateFile) == 0 {
 		config.Usage()
-		os.Exit(1)
+		return 1
 	}
 
 	if _, err := os.Stat(config.TemplateFile); os.IsNotExist(err) {
 		logger.Error("%s not found\n", config.TemplateFile)
-		os.Exit(2)
+		return 2
 	}
 
 	processor, err := tpl.NewTemplateProcessor(config, logger)
 	if err != nil {
 		logger.Error("error creating template processor: %v\n", err)
-		os.Exit(2)
+		return 2
 	}
 
-	defer processor.Close() // ensure resources are cleaned up
+	defer func() {
+		if err := processor.Close(); err != nil {
+			logger.Error("error closing processor: %v\n", err)
+		}
+	}()
+
 	err = processor.RenderTemplate()
 	if err != nil {
 		logger.Error("error rendering template %v: %v\n", config.TemplateFile, err)
-		os.Exit(2)
+		return 2
 	}
+
+	return 0
 }
